624b5eb35d045cccd4e0a6a531f3d199c53c58d5
Horacio G. de Oro <hgdeoro@gmail.com>
2013-12-17 15:46:26 -0300

    Base template + index

--- cachi/urls.py
+++ cachi/urls.py
@@ -7,9 +7,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'cachi.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'cachi.views.index', name='index'),
 
     url(r'^admin/', include(admin.site.urls)),
 )

ed128757cc892cba3ed796fd00799dd933bbaa5a
Carlos Ilcobich <cilcobich@puntoangular.com>
2014-02-19 14:27:45 -0300

    Se agrega utils para funciones generales.

---
+++ cachi/utils.py
@@ -0,0 +1,14 @@
+# encoding: utf-8
+
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+
+def render_html_dinamico(request, html, contexto=None):
+    if contexto == None:
+        contexto = {}
+    return render_to_response(
+        html,
+        contexto,
+        context_instance=RequestContext(request)
+    )

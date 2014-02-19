8797b9d2247dbc0a28072b26f9a61419ac7464c1
Carlos Ilcobich <cilcobich@puntoangular.com>
2014-02-19 14:19:54 -0300

    Se agregan los modelos al Admin de Django.

--- cachi/admin.py
+++ cachi/admin.py
@@ -6,10 +6,18 @@ from cachi import models
 from django.contrib import admin
 
 
-admin.site.register(models.Pieza)
-admin.site.register(models.Color)
-admin.site.register(models.Forma)
-admin.site.register(models.FotografiasDePieza)
+admin.site.register(models.PiezaConjunto)
+admin.site.register(models.Adjunto)
+admin.site.register(models.TipoAdquisicion)
+admin.site.register(models.TipoCondicionHallazgo)
 admin.site.register(models.Naturaleza)
+admin.site.register(models.Persona)
+admin.site.register(models.Ubicacion)
+admin.site.register(models.FichaTecnica)
+admin.site.register(models.DiagnosticoEstadoConservacion)
+admin.site.register(models.InformeCampo)
+admin.site.register(models.UbicacionGeografica)
+admin.site.register(models.Procedencia)
 admin.site.register(models.SitioArqueologico)
-admin.site.register(models.Periodo)
+admin.site.register(models.FichaRelevamientoSitio)
+admin.site.register(models.Modificacion)

d8eebc52157a31d1c4d8679bcfa8fe4a8e1815e3
Horacio G. de Oro <hgdeoro@gmail.com>
2014-03-19 01:44:20 -0300

    Implementa: ignora primera linea de csv

--- cachi/management/commands/importar_sitios_arqueologicos.py
+++ cachi/management/commands/importar_sitios_arqueologicos.py
@@ -2,12 +2,20 @@
 from __future__ import unicode_literals
 
 import csv
+from optparse import make_option
 
 from django.core.management.base import BaseCommand, CommandError
 
 
 class Command(BaseCommand):
     args = 'archivo_csv'
+    option_list = BaseCommand.option_list + (
+        make_option('--ignorar-primer-linea',
+            action='store_true',
+            dest='ignorar_primer_linea',
+            default=False,
+            help='Ignora la primer linea del archivo CSV'),
+        )
 
     def handle(self, *args, **options):
         if len(args) != 1:
@@ -16,6 +24,8 @@ class Command(BaseCommand):
         with open(args[0], 'rb') as f:
             reader_csv = csv.reader(f, dialect=csv.excel_tab)
             rows = [row for row in reader_csv]
+            if options['ignorar_primer_linea']:
+                rows = rows[1:]
             for row in rows:
                 print u" + Row:"
                 for cell in row:
